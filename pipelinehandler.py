f76924707a41987fc74208246abea64c17087392
aws-dojo <90564178+aws-dojo@users.noreply.github.com>
2021-10-06 15:03:48 +0200

    Add files via upload

--- pipelinehandler.py
+++ pipelinehandler.py
@@ -1,3 +1,40 @@
+Job1
+====
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+customerDF = glueContext.create_dynamic_frame.from_catalog(
+             database="dojodb",
+             table_name="src_postgres_public_customers", redshift_tmp_dir="s3://dojo-dataset/scripts/")
+
+glueContext.write_dynamic_frame.from_options(customerDF, connection_type = "s3", connection_options = {"path": "s3://dojo-dataset/raw/customers"}, format = "csv")
+
+Job2
+====
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+customersDF = glueContext.create_dynamic_frame.from_catalog(
+             database="dojodb",
+             table_name="raw_customers")
+glueContext.write_dynamic_frame.from_options(customersDF, connection_type = "s3", connection_options = {"path": "s3://dojo-dataset/cleansed/customers"}, format = "parquet")
+
+Lambda Code
+===========
+
 import json
 import boto3
 
